a92977f3d63f1bf5eabb7b57bdcfaef3785ae900
muneebsaddal <muneebsaddal@outlook.com>
2022-02-12 15:31:35 +0500

    form section complete

---
+++ meme-generator/src/components/Meme.js
@@ -0,0 +1,14 @@
+
+function Meme() {
+    return (
+        <main>
+            <form className="form">
+                <input type="text" placeholder="Top text" className="form-input"></input>
+                <input type="text" placeholder="Bottom text" className="form-input"></input>
+                <button className="form-button">get a new meme</button>
+            </form>
+        </main>
+    )
+}
+
+export default Meme
