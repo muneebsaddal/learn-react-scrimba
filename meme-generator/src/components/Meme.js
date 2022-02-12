007f17886029fdc0710c091dadc433b4730d0901
muneebsaddal <muneebsaddal@outlook.com>
2022-02-12 18:51:05 +0500

    meme image updated using states

--- meme-generator/src/components/Meme.js
+++ meme-generator/src/components/Meme.js
@@ -1,12 +1,26 @@
+import { React, useState } from "react"
+import memesData from "../memesData"
 
 function Meme() {
+
+    const [memeImage, setMemeImage] = useState("")
+
+    function getMemeImage() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        setMemeImage(memesArray[randomNumber].url)
+        console.log(memeImage)
+        
+    }
+
     return (
         <main>
-            <form className="form">
+            <div className="form">
                 <input type="text" placeholder="Top text" className="form-input"></input>
                 <input type="text" placeholder="Bottom text" className="form-input"></input>
-                <button className="form-button">get a new meme</button>
-            </form>
+                <button className="form-button" onClick={getMemeImage}>get a new meme</button>
+            </div>
+            <img src={memeImage} className="main-image"></img>
         </main>
     )
 }
